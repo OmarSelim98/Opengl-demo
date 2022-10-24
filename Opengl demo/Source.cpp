38ead7f6eac4fd0b7830a97173df05813f85c09f
Omar Selim <omarselim98@gmail.com>
2022-10-24 21:39:47 +0200

    Rectangle

--- Opengl demo/Source.cpp
+++ Opengl demo/Source.cpp
@@ -23,13 +23,13 @@ float vertices[] = {
 	-0.5f, -0.5f, 0.0f,	// bottom left
 	0.5f, -0.5f, 0.0f,	// bottom right
 	0.5f, 0.5f, 0.0f,	//top right
-	//-0.5f, 0.5f, 0.0f	//top left
+	-0.5f, 0.5f, 0.0f	//top left
 };
-//
-//unsigned int indices[] = {
-//	0 , 1 , 2,
-//	0 , 2 , 3
-//};
+
+unsigned int indices[] = {
+	0 , 1 , 2,
+	0 , 2 , 3
+};
 
 unsigned int VAO;
 unsigned int VBO;
@@ -123,9 +123,9 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	//elements buffer object
-	/*glGenBuffers(1, &EBO);
+	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//set & enable vertex attributes
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -133,11 +133,11 @@ int main() {
 
 	glBindBuffer(GL_ARRAY_BUFFER,0); // It is safe here to unbind VBO before unbinding VAO, as glVertexAttribPointer already registers the vertex attribute's bound buffer (VBO in this case).
 	glBindVertexArray(0);
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	/* ========== */
 
 	/* Render Config */
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //Wireframe polygons
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //Wireframe polygons
 	/* ========== */
 
 	//Loop : Swap Buffers and Check events.
@@ -149,8 +149,8 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0); // use EBO to draw triangles.
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0); // use EBO to draw triangles.
 		glBindVertexArray(0);
 
 		// Rendering commands end
