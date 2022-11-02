6a904a2f072e69a016722103da5579c9d74000b2
Omar Selim <omarselim98@gmail.com>
2022-11-02 14:54:55 +0200

    3

--- Opengl demo/Source.cpp
+++ Opengl demo/Source.cpp
@@ -11,10 +11,10 @@ float BACKGROUND_COLOR[] = { 0.0f, 0.0f, 0.0f, 1.0f };
 
 float vertices[] = {
 	//pos			 //color				//texcoords
-	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f,   // top right
-	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f,   // bottom right
-	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f    // top left
+	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.5f, 0.5f,   // top right
+	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.5f, 0.5f,   // bottom right
+	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.5f, 0.5f,   // bottom left
+	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.5f, 0.5f    // top left
 };
 
 unsigned int indices[] = {
